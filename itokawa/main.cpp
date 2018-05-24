fd3ee922528ddf4cbe872771ecb742aa878508d6
JoseCPI <josecarlos.pimienta@gmail.com>
2018-05-24 14:21:16 -0600

    programa terminado
    el programa ya imprime los puntos de los objetos

--- itokawa/main.cpp
+++ itokawa/main.cpp
@@ -2,6 +2,8 @@
 * 22 de mayo del 2018
 * Programa que lee los puntos de triangulos de un archivo
 * y los almacena en estructuras tipo triangulo para graficarlos.
+
+    mingw32-make.exe
 */
 
 #include <cstdlib>
@@ -9,204 +11,277 @@
 #include <fstream>
 #include <new>
 #include <iomanip>
+//#include <SDL2/SDL.h>
 #include <SDL.h>
 #include <windows.h>
+#include <math.h>
 
 using namespace std;
-//*************************************
-
-struct punto{
+//*****************************************************************
+struct Punto{
     double x;
     double y;
     double z;
 };
-//**************************************
-punto viewport ( float, float, float, float, float, float);
-void Linea(SDL_Renderer* ,int);
-//*************************************
+
+struct Paq{
+    Punto B,C;
+};
+//*******************************************************************
+void viewport(Punto* P, int cantidad, int caso);
+void POrtograficaXY(SDL_Renderer* renderer, Punto* p, int cantidad);
+void POrtograficaXZ(SDL_Renderer* renderer, Punto* p, int cantidad);
+void POrtograficaYZ(SDL_Renderer* renderer, Punto* p, int cantidad);
+void PAxonometrica(SDL_Renderer* renderer, Punto* p, int cantidad);
+Paq calcularMaxMin(Punto* A, int cantidad);
+int menu(void);
+//********************************************************************
 int main(int argc, char* args[]){
+    ifstream archivo;
+    int puntos;
+    SDL_Event e;
+    int limite=800;
 
-  ifstream archivo;
-  int puntos;
-  SDL_Event e;
-  int limite=800;
+    int op = menu();
 
-  if (!archivo.is_open()) //check is file has been opened
-  {
-      archivo.open ("itokawa_f0049152.tri", ios::in | ios::out);
+    if (op==0) return 0;
 
-      if (!archivo)
-      {
-          cerr << "Failed to open " << archivo << endl;
-          exit(EXIT_FAILURE);  //abort program
-      }
-  }
+    if (!archivo.is_open()) //check is file has been opened
+    {
+        if(op==1) archivo.open ("itokawa_f0049152.tri", ios::in | ios::out);
+        if(op==2) archivo.open ("skull.tri", ios::in | ios::out);
 
-  archivo >> puntos;
-  punto* p= new punto[puntos*3];
+        if (!archivo)
+        {
+            cerr << "Failed to open " << archivo << endl;
+            exit(EXIT_FAILURE);  //abort program
+        }
+    }
 
-  for (int i=0; i<puntos *3; i++){
+    archivo >> puntos;
+    Punto* p = new Punto[puntos*3];
+
+    for (int i=0; i<puntos *3; i++){
         archivo >> p[i].x;
         archivo >> p[i].y;
         archivo >> p[i].z;
-  }
-  archivo.close();
-
-  cout<<p[(puntos*3)-1].x<<endl;
-  cout<<p[(puntos*3)-1].y<<endl;
-  cout<<p[(puntos*3)-1].z<<endl;
-
+    }
+    archivo.close();
 
     if (SDL_Init(SDL_INIT_VIDEO) == 0) {
         SDL_Event event;
         SDL_Window* window = NULL;
         SDL_Renderer* renderer = NULL;
-      if (SDL_CreateWindowAndRenderer(limite,limite, 0, &window, &renderer) == 0) {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-                SDL_RenderClear(renderer);
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                SDL_RenderDrawLine(renderer, 300,0,300,600);
-                SDL_RenderDrawLine(renderer, 0,300,600,300);
-                SDL_SetRenderDrawColor(renderer, 255, 0,0, 0);
+        if (SDL_CreateWindowAndRenderer(limite, limite, 0, &window, &renderer) == 0) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderDrawLine(renderer, 400, 0, 400,800); // linea vertical
+            SDL_RenderDrawLine(renderer, 0,400,800,400); // linea horizontal
+            SDL_SetRenderDrawColor(renderer, 255, 0,0, 0);
 
+            // itokawa
+            POrtograficaXY(renderer, p, puntos*3);
+            POrtograficaXZ(renderer, p, puntos*3);
+            POrtograficaYZ(renderer, p, puntos*3);
+            PAxonometrica(renderer, p, puntos*3);
+            //************
 
-                for (int i=0; i<puntos; i++){
-                //    p1= viewport() ///  CONTINUAR AQUI
-                }
+            SDL_RenderPresent(renderer);
+            while (1) {
+                if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+                break;
+            }
 
-
-                SDL_RenderPresent(renderer);
-                while (1) {
-                    if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-                    break;
-                }
-
-                if (renderer) {
-                    SDL_DestroyRenderer(renderer);
-                }
-                if (window) {
-                    SDL_DestroyWindow(window);
-                }
+            if (renderer) {
+                SDL_DestroyRenderer(renderer);
+            }
+            if (window) {
+                SDL_DestroyWindow(window);
             }
         }
+    }
 
     SDL_Quit();
 
+    delete[] p;
 
-  delete[] p;
+    return 0;
+}
+//**************************************************************
+Paq calcularMaxMin(Punto* A, int cantidad){
+    Paq H;
+    Punto B;
+    Punto C;
 
-  return 0;
+    double max_x=A->x;
+    double max_y=A->y;
+    double max_z=A->z;
+    double min_x=A->x;
+    double min_y=A->y;
+    double min_z=A->z;
+    A++;
+    for(int i=1; i<cantidad; i++, A++){
+        if(A->x > max_x) max_x = A->x;
+        if(A->y > max_y) max_y = A->y;
+        if(A->z > max_z) max_z = A->z;
+        if(A->x < min_x) min_x = A->x;
+        if(A->y < min_y) min_y = A->y;
+        if(A->z < min_z) min_z = A->z;
+    }
+
+    B.x = max_x;
+    B.y = max_y;
+    B.z = max_z;
+
+    C.x = min_x;
+    C.y = min_y;
+    C.z = min_z;
+
+    H.B = B;
+    H.C = C;
+    return H;
+}
+//********************************************************************************
+void copiar(Punto* P, Punto* Q,int cantidad){
+    for(int i=0; i < cantidad; i++, Q++, P++){
+        Q->x = P->x;
+        Q->y = P->y;
+        Q->z = P->z;
+    }
+}
+//********************************************************************************
+void viewport(Punto* P, int cantidad, int caso){
+    Paq X = calcularMaxMin(P, cantidad);
+
+    double Rx = X.B.x - X.C.x;
+    double Ry = X.B.y - X.C.y;
+    double Rz = X.B.z - X.C.z;
+    double Cx, Cy, Cz;
+
+    switch(caso){
+        case 0: //xy
+            if(Rx>Ry) Ry=Rx;
+            else Rx=Ry;
+            Cx = 360*X.C.x/Rx;
+            Cy = 360*X.C.y/Ry;
+
+            for(int i=0; i <cantidad; i++, P++){
+                P->x = P->x*360/Rx - Cx + 20;
+                P->y = P->y*360/Ry - Cy + 20;
+            }
+            break;
+        case 1: //xz
+            if(Rx>Rz) Rz=Rx;
+            else Rx=Rz;
+            Cx = 360*X.C.x/Rx;
+            Cz = 360*X.C.z/Rz;
+            for(int i=0; i <cantidad; i++, P++){
+                P->x = P->x*360/Rx - Cx + 20;
+                P->z = P->z*360/Rz - Cz + 420;
+            }
+            break;
+        case 2: //yz
+            if(Ry>Rz) Rz=Ry;
+            else Ry=Rz;
+            Cy = 360*X.C.y/Ry;
+            Cz = 360*X.C.z/Rz;
+            for(int i=0; i <cantidad; i++, P++){
+                P->y = P->y*360/Ry -Cy + 420;
+                P->z = P->z*360/Rz -Cz + 20;
+            }
+            break;
+        case 3://axonometrica
+
+            if(Rx>Ry) Ry=Rx;
+            else Rx=Ry;
+            Cx = 360*X.C.x/Rx;
+            Cy = 360*X.C.y/Ry;
+            for(int i=0; i <cantidad; i++, P++){
+                P->x = P->x*360/Rx - Cx + 420;
+                P->y = P->y*360/Ry - Cy + 420;
+            }
+
+    }
 
 }
-//************************************************************
-//************************************************************
-
-//************************************************************
-
-//**********************************************************************
-void Linea(SDL_Renderer* renderer,int limite){
-
-    int X1,Y1,X2,Y2,aux;
-    double dY, dX,k,X,Y,av,avR,avI;
-    int IncYi,IncXi,IncXr,IncYr;
-
-    cout << endl << "Linea (algoritmo punto medio)" << endl;
-    do{
-        cout << "Ingrese valor de x1: ";
-        cin >> X1;
-        if(X1>limite){
-            cout << endl << "El valor de x1 excede el rango";
-            Sleep(1200);
-            system("cls");
-        }
-
-    }while(X1>limite);
-
-    do{
-        cout << "Ingrese valor de y1: ";
-        cin >> Y1;
-        if(Y1>limite){
-            cout << endl << "El valor de y1 excede el rango";
-            Sleep(1200);
-            system("cls");
-        }
-
-    }while(Y1>limite);
-
-    do{
-        cout << "Ingrese valor de x2: ";
-        cin >> X2;
-        if(X2>limite){
-            cout << endl << "El valor de x2 excede el rango";
-            Sleep(1200);
-            system("cls");
-        }
-
-    }while(X2>limite);
-
-    do{
-        cout << "Ingrese valor de y2: ";
-        cin >> Y2;
-        if(Y2>limite){
-            cout << endl << "El valor de y2 excede el rango";
-            Sleep(1200);
-            system("cls");
-        }
-
-    }while(Y2>limite);
-
-    if(X2<X1){
-
-        aux=X2;
-        X2=X1;
-        X1=aux;
-
-        aux=Y2;
-        Y2=Y1;
-        Y1=aux;
-    }
-
-    dY=(Y2-Y1);
-    dX=(X2-X1);
-     // 1 - Incrementos para las secciones con avance inclinado
-    if(dY>=0) IncYi = 1;
-    else IncYi = -1;
-
-    if(dX>=0)IncXi = 1;
-    else IncXi = -1;
-
-    // 2 - Incrementos para las secciones con avance recto:
-    if(dX>=dY){
-        IncYr = 0;
-        IncXr = IncXi;
-    }else{
-        IncXr = 0;
-        IncYr = IncYi;
-        // Cuando dy es mayor que dx, se intercambian, para reutilizar el mismo bucle.
-        k = dX;
-        dX = dY;
-        dY = k;
-    }
-
-    // 3  - Inicializar valores (y de error).
-    X = X1;
-    Y = Y1;
-    avR = (2 * dY);
-    av = (avR - dX);
-    avI = (av - dX);
-
-    // 4  - Bucle para el trazado de las l�nea
-        while(X<=X2){
-            SDL_RenderDrawPoint(renderer, X+300, -Y+300);
-            if(av>=0){
-                X = (X + IncXi);     // X aumenta en inclinado.
-                Y = (Y + IncYi);     // Y aumenta en inclinado.
-                av = (av + avI);     // Avance Inclinado
-            }else{
-                X = (X + IncXr);     // X aumenta en recto.
-                Y = (Y + IncYr);     // Y aumenta en recto.
-                av = (av + avR);     // Avance Recto
+//********************************************************************************
+void pintarPuntos(SDL_Renderer* renderer, Punto* P, int cantidad, int caso){
+    switch(caso){
+        case 0: // xy
+            for(int i=0; i <cantidad; i++, P++){
+                SDL_RenderDrawPoint(renderer, int(P->x), int(P->y));
             }
-        }
+            break;
+        case 1: // xz
+            for(int i=0; i <cantidad; i++, P++){
+                SDL_RenderDrawPoint(renderer, int(P->x), int(P->z));
+            }
+            break;
+        case 2: // yz
+            for(int i=0; i <cantidad; i++, P++){
+                SDL_RenderDrawPoint(renderer, int(P->y), int(P->z));
+            }
+            break;
+    }
+}
 
+void POrtograficaXY( SDL_Renderer* renderer, Punto* p, int cantidad){
+    //graficar entre los pixeles 0-400 en x y 0-400 en y
+    Punto *q = new Punto[cantidad];
+    copiar(p,q,cantidad);
+
+    viewport(q,cantidad,0); //guarda en q los nuevos puntos
+    pintarPuntos(renderer, q, cantidad, 0);
+}
+
+void POrtograficaXZ(SDL_Renderer* renderer, Punto* p, int cantidad){
+    //graficar entre los pixeles 0-400 en x y 400-800 en z
+    Punto *q = new Punto[cantidad];
+    copiar(p, q, cantidad);
+    viewport(q, cantidad, 1); //guarda en q los nuevos puntos
+    pintarPuntos(renderer, q, cantidad, 1);
+}
+
+void POrtograficaYZ( SDL_Renderer* renderer, Punto* p, int cantidad){
+    //graficar entre los pixeles 400-800 en x y 0-400 en z
+    Punto *q = new Punto[cantidad];
+    copiar(p, q, cantidad);
+    viewport(q, cantidad, 2); //guarda en q los nuevos puntos
+    pintarPuntos(renderer, q, cantidad, 2);
+}
+
+
+void PAxonometrica(SDL_Renderer* renderer, Punto* p, int cantidad){
+    //graficar entre los pixeles 400-800 en x y 400-800 en y
+    Punto *q,*q2;
+
+    q = new Punto[cantidad]; //nuevo arreglo de puntos modificados por la proyección
+    q2 = q;
+
+    //CALCULO DE PROYECCION
+    for(int i=0; i<cantidad ; i++,q2++,p++){
+        q2->x = p->x*(1/sqrt(2)) - p->z/sqrt(2);
+        q2->y = p->x*-1/(sqrt(2)*sqrt(3)) + p->y*(sqrt(2))/(sqrt(3)) - p->z/(sqrt(2)*sqrt(3));
+        q2->z = 0;
+    }
+
+    viewport(q, cantidad, 3);
+    pintarPuntos(renderer, q, cantidad, 0);
+}
+
+//********************************************************************************************
+int menu(){
+    int op;
+
+    do{
+        cout<<"Elige una opci\xA2n:\n";
+        cout<<"1) Itokawa\n";
+        cout<<"2) Craneo\n";
+        cout<<"0) Salir\n";
+        cout<<"\n Opci\xA2n: ";
+        cin>>op;
+    }while(op != 1 && op!= 2 && op!=0);
+
+    return (op);
 }
